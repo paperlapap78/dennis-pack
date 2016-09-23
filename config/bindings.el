6cdeaae04efb592dc7cae4305a5ef294c74c29c4
dprobst <dprobst@dius.com.au>
2016-09-23 13:48:14 +1000

    First commit

---
+++ config/bindings.el
@@ -0,0 +1,13 @@
+;; Place your bindings here.
+
+;; For example:
+;;(define-key global-map (kbd "C-+") 'text-scale-increase)
+;;(define-key global-map (kbd "C--") 'text-scale-decrease)
+
+
+(require 'multiple-cursors)
+
+(global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
+(global-set-key (kbd "C->") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
